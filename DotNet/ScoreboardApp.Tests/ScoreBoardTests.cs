f6372f14372ff13f6d87e0a8595250ebbbc8871f
Aakash Apoorv <aakashapoorv@users.noreply.github.com>
2023-07-22 18:04:25 +0200

    test: added failing test for StartMatch method

--- DotNet/ScoreboardApp.Tests/ScoreBoardTests.cs
+++ DotNet/ScoreboardApp.Tests/ScoreBoardTests.cs
@@ -13,5 +13,15 @@ namespace ScoreboardApp.Tests
         {
             _scoreBoard = new Scoreboard();
         }
+
+        [Test]
+        public void StartMatch_NewMatch_ReturnsCorrectMatch()
+        {
+            var match = _scoreBoard.StartMatch("Team A", "Team B");
+            Assert.AreEqual("Team A", match.HomeTeam);
+            Assert.AreEqual("Team B", match.AwayTeam);
+            Assert.AreEqual(0, match.HomeScore);
+            Assert.AreEqual(0, match.AwayScore);
+        }
     }
 }
--- TypeScript/scoreboard.test.ts
+++ TypeScript/scoreboard.test.ts
@@ -7,4 +7,12 @@ describe('ScoreBoard', () => {
   beforeEach(() => {
     scoreBoard = new ScoreBoard();
   });
+
+  test('start a new match', () => {
+    const match = scoreBoard.startMatch('Team A', 'Team B');
+    expect(match.homeTeam).toEqual('Team A');
+    expect(match.awayTeam).toEqual('Team B');
+    expect(match.homeScore).toEqual(0);
+    expect(match.awayScore).toEqual(0);
+  });
 });
